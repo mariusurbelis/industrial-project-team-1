010a0e0d44f775f910b256cef74890dacbe952b7
Marius Urbelis <marius@urbelis.dev>
2020-10-08 13:57:37 +0100

    Room join area

---
+++ Assets/Scripts/RoomJoinArea.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomJoinArea : MonoBehaviour
+{
+    [SerializeField] private GameObject gameStartButton;
+
+    private int maxPlayers = 2;
+
+    private int playersInsideTheArea = 0;
+
+    private void Update()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            gameObject.SetActive(playersInsideTheArea == maxPlayers);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playersInsideTheArea++;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playersInsideTheArea--;
+        }
+    }
+}
