de32ea69c4ccf1928da17a23ea0f0e46cbd3d4e9
Marius Urbelis <marius@urbelis.dev>
2020-10-14 14:25:10 +0100

    Updated menu UI

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button joinButton = null;
+    [SerializeField] private Button hostButton = null;
+    [SerializeField] private Button settingsButton = null;
+    [SerializeField] private Button backButton = null;
+    [SerializeField] private GameObject navigationPanel = null;
+    [SerializeField] private GameObject userPanel = null;
+    [SerializeField] private GameObject hostPanel = null;
+   
+    private void Start()
     {
-        
+        joinButton.onClick.AddListener(() => { ShowScreen(userPanel); });
+        hostButton.onClick.AddListener(() => { ShowHostScreen(); });
+        backButton.onClick.AddListener(() => { ShowScreen(navigationPanel); });
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ShowHostScreen()
     {
-        
+        ShowScreen(userPanel);
+        hostPanel.SetActive(true);
+    }
+
+    private void ShowScreen(GameObject activeObject)
+    {
+        HideUI();
+        activeObject.SetActive(true);
+    }
+
+    private void HideUI()
+    {
+        navigationPanel.SetActive(false);
+        userPanel.SetActive(false);
+        hostPanel.SetActive(false);
     }
 }
