f7212f6ea6d0006294655d680af1eb1a2cb451a8
Marius Urbelis <marius@urbelis.dev>
2020-10-14 14:52:58 +0100

    Merge (#64)
    * Code update (#61)
    
    * First reference comment in QuestionManager.cs
    
    * Slight refactoring and documentation for Helpers, Networking and Player
    
    * Added comments to Quiz folder's scripts.
    
    * Changes to the QuizManager.cs
    
    * RoomJoinArea.cs commented
    
    * Added more comments.
    
    Co-authored-by: Costin <costingrigore6@gmail.com>
    
    Co-authored-by: Costin <costingrigore6@gmail.com>
    
    * Added some UI elements to Connection Scene.
    
    * Created a new script for Menu Management.
    
    * Updated menu UI
    
    Co-authored-by: Costin <costingrigore6@gmail.com>

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private Button joinButton = null;
+    [SerializeField] private Button hostButton = null;
+    [SerializeField] private Button settingsButton = null;
+    [SerializeField] private Button backButton = null;
+    [SerializeField] private GameObject navigationPanel = null;
+    [SerializeField] private GameObject userPanel = null;
+    [SerializeField] private GameObject hostPanel = null;
+   
+    private void Start()
+    {
+        joinButton.onClick.AddListener(() => { ShowScreen(userPanel); });
+        hostButton.onClick.AddListener(() => { ShowHostScreen(); });
+        backButton.onClick.AddListener(() => { ShowScreen(navigationPanel); });
+    }
+
+    private void ShowHostScreen()
+    {
+        ShowScreen(userPanel);
+        hostPanel.SetActive(true);
+    }
+
+    private void ShowScreen(GameObject activeObject)
+    {
+        HideUI();
+        activeObject.SetActive(true);
+    }
+
+    private void HideUI()
+    {
+        navigationPanel.SetActive(false);
+        userPanel.SetActive(false);
+        hostPanel.SetActive(false);
+    }
+}
