58011da0d520a6d6a02ded48756f1891929ff982
ch6li3 <34590149+ch6li3@users.noreply.github.com>
2020-10-15 10:57:42 +0100

    create instrustions pop up done

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -10,15 +10,22 @@ public class MenuController : MonoBehaviour
     [SerializeField] private Button hostButton = null;
     [SerializeField] private Button settingsButton = null;
     [SerializeField] private Button backButton = null;
+    [SerializeField] private Button instructionsButton = null;
+    [SerializeField] private Button closeInstructionsButton = null;
     [SerializeField] private GameObject navigationPanel = null;
     [SerializeField] private GameObject userPanel = null;
     [SerializeField] private GameObject hostPanel = null;
-   
+    [SerializeField] private GameObject instructionsPopUP = null;
+    [SerializeField] private GameObject gameName = null;
+    [SerializeField] private GameObject lobbyInfo = null;
+
     private void Start()
     {
         joinButton.onClick.AddListener(() => { ShowScreen(userPanel); });
         hostButton.onClick.AddListener(() => { ShowHostScreen(); });
         backButton.onClick.AddListener(() => { ShowScreen(navigationPanel); });
+        instructionsButton.onClick.AddListener(() => { ShowInstructions(); });
+        closeInstructionsButton.onClick.AddListener(() => { ShowScreen(navigationPanel); });
     }
 
     private void ShowHostScreen()
@@ -27,16 +34,36 @@ public class MenuController : MonoBehaviour
         hostPanel.SetActive(true);
     }
 
+    /**
+   * Method to hide display panels based.
+   */
     private void ShowScreen(GameObject activeObject)
     {
         HideUI();
         activeObject.SetActive(true);
     }
 
+
+    /**
+     * Method to hide no relevant UI panels 
+     */
     private void HideUI()
     {
         navigationPanel.SetActive(false);
         userPanel.SetActive(false);
         hostPanel.SetActive(false);
+        instructionsPopUP.SetActive(false);
+   
+    }
+
+    /**
+     * Method to display the instructions panel while hiding the other non relevant panels
+     */
+    private void ShowInstructions()
+    {
+        HideUI();
+        instructionsPopUP.SetActive(true);
+      
     }
 }
+
