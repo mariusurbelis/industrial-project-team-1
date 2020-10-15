208936357afd1a704c71e6273880debab8473e45
ch6li3 <34590149+ch6li3@users.noreply.github.com>
2020-10-15 13:46:07 +0100

    Instructions created (#70)
    * Merge (#64)
    
    * Code update (#61)
    
    * First reference comment in QuestionManager.cs
    
    * Slight refactoring and documentation for Helpers, Networking and Player
    
    * Added comments to Quiz folder's scripts.
    
    * Changes to the QuizManager.cs
    
    * RoomJoinArea.cs commented
    
    * Added more comments.
    
    Co-authored-by: Costin <costingrigore6@gmail.com>
    
    Co-authored-by: Costin <costingrigore6@gmail.com>
    
    * Added some UI elements to Connection Scene.
    
    * Created a new script for Menu Management.
    
    * Updated menu UI
    
    Co-authored-by: Costin <costingrigore6@gmail.com>
    
    * create instrustions pop up done
    
    * Fixed anchoring the buttons
    
    Co-authored-by: Marius Urbelis <marius@urbelis.dev>
    Co-authored-by: Costin <costingrigore6@gmail.com>

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
@@ -7,15 +9,22 @@ public class MenuController : MonoBehaviour
     [SerializeField] private Button hostButton = null;
     [SerializeField] private Button settingsButton = null;
     [SerializeField] private Button backButton = null;
+    [SerializeField] private Button instructionsButton = null;
+    [SerializeField] private Button closeInstructionsButton = null;
     [SerializeField] private GameObject navigationPanel = null;
     [SerializeField] private GameObject userPanel = null;
     [SerializeField] private GameObject hostPanel = null;
-   
+    [SerializeField] private GameObject instructionsPopUP = null;
+    [SerializeField] private GameObject gameName = null;
+    [SerializeField] private GameObject lobbyInfo = null;
+
     private void Start()
     {
         joinButton.onClick.AddListener(() => { ShowScreen(userPanel); });
         hostButton.onClick.AddListener(() => { ShowHostScreen(); });
         backButton.onClick.AddListener(() => { ShowScreen(navigationPanel); });
+        instructionsButton.onClick.AddListener(() => { ShowInstructions(); });
+        closeInstructionsButton.onClick.AddListener(() => { ShowScreen(navigationPanel); });
     }
 
     private void ShowHostScreen()
@@ -35,5 +44,15 @@ public class MenuController : MonoBehaviour
         navigationPanel.SetActive(false);
         userPanel.SetActive(false);
         hostPanel.SetActive(false);
+        instructionsPopUP.SetActive(false);
+   
+    }
+
+    private void ShowInstructions()
+    {
+        HideUI();
+        instructionsPopUP.SetActive(true);
+      
     }
 }
+
