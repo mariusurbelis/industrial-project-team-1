32eeeb3fe7ff18e33ada8e9cdca6ad24dc00c34f
Costin <costingrigore6@gmail.com>
2020-10-15 14:36:52 +0100

    Added Sound class

---
+++ Assets/Scripts/Sound/Sound.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace SquirrelForest
+{
+    public class Sound : MonoBehaviour
+    {
+        public static AudioClip dieSound;
+
+        public AudioClip dieSoundStub;
+
+        public static AudioSource audioSource;
+
+        private void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+
+            dieSound = dieSoundStub;
+
+            if (!FindObjectOfType<AudioListener>())
+            {
+                gameObject.AddComponent<AudioListener>();
+            }
+        }
+
+        public static void PlaySound(AudioClip clip)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+    }
+}
