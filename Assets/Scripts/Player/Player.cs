8a945c48b4ba435a0c93d61cbc7e353f2cfb8e52
Marius Urbelis <marius@urbelis.dev>
2020-10-16 19:53:05 +0100

    Powerup development

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -81,6 +81,7 @@ public class Player : MonoBehaviour
 			case Powerup.PowerupType.Bomb:
 				//myBody.AddForce(powerupPosition - (Vector2)transform.position, ForceMode2D.Impulse);
 				myBody.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+				transform.position += Vector3.right * 5f;
 				break;
 		}
 	}
