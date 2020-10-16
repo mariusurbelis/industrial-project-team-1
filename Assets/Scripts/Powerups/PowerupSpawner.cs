aa0f3816da4176693ae61da3783649270d36f757
ch6li3 <34590149+ch6li3@users.noreply.github.com>
2020-10-16 19:09:38 +0100

    Powerup development

--- Assets/Scripts/Powerups/PowerupSpawner.cs
+++ Assets/Scripts/Powerups/PowerupSpawner.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class PowerupSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [System.Serializable]
+    public class powerUp
     {
-        
+        public string name;
+        public GameObject prefab;
+        public float chance;
+        public float delayTime;
     }
 
-    // Update is called once per frame
+    public Transform[] points;
+    public powerUp[] powerUps;
+    public int SpawnTimer;
+
+    powerUp chosenPowerUp;
+    float random;
+    float cumulative;
+
+    void Start()
+    {
+        SpawnTimer = 0;
+    }
+
     void Update()
     {
-        
+        SpawnTimer = SpawnTimer + 1;
+        if (SpawnTimer >= 100)
+        {
+            SpawnRandom();
+            SpawnTimer = 0;
+        }
+    }
+
+    void SpawnRandom()
+    {
+        random = Random.value;
+        cumulative = 0f;
+
+        for (int i = 0; i < powerUps.Length; i++)
+        {
+            cumulative +=powerUps[i].chance;
+            if (random < cumulative && Time.time >= powerUps[i].delayTime)
+            {
+                 chosenPowerUp=powerUps[i];
+                break;
+            }
+        }
+
+        Instantiate(chosenPowerUp.prefab, points[Random.Range(0, points.Length)].position, points[Random.Range(0, points.Length)].rotation);
     }
 }
