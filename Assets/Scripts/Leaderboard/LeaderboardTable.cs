4a12a015484a1fe0f40d2c6fa6828485cd31c0b7
Jodie <jmlaurenson@dundee.ac.uk>
2020-10-17 15:32:39 +0100

    Fixed formatting of leaderboard and scalability

--- Assets/Scripts/Leaderboard/LeaderboardTable.cs
+++ Assets/Scripts/Leaderboard/LeaderboardTable.cs
@@ -5,11 +5,9 @@ using UnityEngine.UI;
 
 public class LeaderboardTable : MonoBehaviour
 {
-    public int numberOfPlayers;
     public GameObject templatePosition;
     GameObject[] positionClone;
     public Text[] nicknames;
-    public GameObject canvas;
     List<GameObject> clonesArray = new List<GameObject>();
 
 
@@ -19,67 +17,79 @@ public class LeaderboardTable : MonoBehaviour
     /// </summary>
     void Start()
     {
-        numberOfPlayers = QuizManager.eliminationList.Count;
+        int numberOfPlayers = QuizManager.eliminationList.Count;
         positionClone = new GameObject[numberOfPlayers+1];
-        nicknames = new Text[numberOfPlayers];
+        nicknames = new Text[numberOfPlayers+1];
         
-        createClones();
-        fillLeaderboard();
+        createClones(numberOfPlayers);
+        fillLeaderboard(numberOfPlayers);
 
     }
 
-    void fillLeaderboard()
+    /// <summary>
+    /// Fills the leaderboard with all of the players names
+    /// </summary>
+    /// <param name="numberOfPlayers">number of players in the room</param>
+    void fillLeaderboard(int numberOfPlayers)
     {
-        for(int i = 0; i < numberOfPlayers; i++)
+        int counter = 0;
+        for (int i = numberOfPlayers - 1; i >= 0; i--)
         {
-            setNickname(i, QuizManager.eliminationList[i]);
+            setNickname(counter, QuizManager.eliminationList[i]);
+            counter++;
         }
     }
 
     /// <summary>
-    ///This function instantiates the clones and formats them depending on the amount of players
-    ///</summary>
-    void createClones()
+    /// This function instantiates the clones and formats the positions depending on the amount of players
+    /// </summary>
+    /// <param name="numberOfPlayers">number of players in the room</param>
+    void createClones(int numberOfPlayers)
     {
-        if (numberOfPlayers < 10) //if true, keep the positions in one colomn
+        if (numberOfPlayers <= 10) //if true, keep the positions in one colomn
         {
-            Debug.Log("Players less than 10");
             for (int i = numberOfPlayers-1; i >=0; i--)
             {
-                GameObject.Find("Position").GetComponent<Text>().text = (i+1).ToString() + "th";
+                setPosition(i, GameObject.Find("Position").GetComponent<Text>());
                 positionClone[i] = Instantiate(templatePosition) as GameObject; // clone position
-                positionClone[i].transform.SetParent(templatePosition.transform.parent);
-                positionClone[i].transform.Translate(512, 383 - (i * 40), 0);
+                positionClone[i].transform.SetParent(templatePosition.transform.parent); //change dimensions of clones to match the template
+
+                positionClone[i].GetComponent<RectTransform>().localScale = templatePosition.GetComponent<RectTransform>().localScale;
+                positionClone[i].transform.position = templatePosition.transform.position;
+
+                positionClone[i].transform.Translate(0,0-(i * 15), 0);
             }
         }
         else //if true, then format the positions so that it is in two columns instead of one
         {
-            templatePosition.transform.Translate(-250, 0, 0);
             for (int i = numberOfPlayers-1 ; i >=0; i--)
             {
+                setPosition(i, GameObject.Find("Position").GetComponent<Text>());
 
-                GameObject.Find("Position").GetComponent<Text>().text = (i+1).ToString()+"th";
-                
                 positionClone[i] = Instantiate(templatePosition) as GameObject; //clone position
                 positionClone[i].transform.SetParent(templatePosition.transform.parent);
+                positionClone[i].GetComponent<RectTransform>().localScale = templatePosition.GetComponent<RectTransform>().localScale;
+                positionClone[i].transform.position = templatePosition.transform.position;
                 if (i < 10) //column one
                 {
-                    positionClone[i].transform.Translate(512, 383 - (i * 50), 0);
+                    positionClone[i].transform.Translate(-90, 0 - (i * 15), 0);
                 }
                 else //column two
                 {
-                    positionClone[i].transform.Translate(1020,383 - ((i-10) * 50), 0);
+
+                    positionClone[i].transform.Translate(90, 0 - ((i-10) * 15), 0);
                 }
-                
             }
-            positionClone[numberOfPlayers - 1].transform.Translate(-250, 0, 0);
         }
+        templatePosition.transform.Translate(0, 1000, 0);
         addClonesToArray(); 
     }
 
     /// <summary>
-    ///Sets the nickname at the given position
-    ///</summary>
+    /// Sets the nickname at the given position
+    /// </summary>
+    /// <param name="position">The position on the leaderboard thats being edited</param>
+    /// <param name="name">The nickname of the player</param>
     void setNickname(int position, string name)
     {
         clonesArray[position].transform.Find("nickname").GetComponent<Text>().text = name;
@@ -94,13 +104,30 @@ public class LeaderboardTable : MonoBehaviour
     ///</summary>
     void addClonesToArray()
     {
-        for (int j = canvas.transform.childCount - 1; j >= 0; j--)
+        for (int j = this.transform.childCount - 1; j >= 0; j--)
         {
-            clonesArray.Add(canvas.transform.GetChild(j).gameObject);
+            clonesArray.Add(this.transform.GetChild(j).gameObject);
         }
     }
 
-    
+    /// <summary>
+    /// Changes the text for each position to show the correct placings
+    /// </summary>
+    /// <param name="pos">the position to be added to the leaderboard</param>
+    /// <param name="textObject">the text object that is being edited</param>
+    void setPosition(int pos, Text textObject)
+    {
+        textObject.text = (pos + 1).ToString();
+        if (pos == 0)
+            textObject.text += "st";
+        else if (pos == 1)
+            textObject.text += "nd";
+        else if (pos == 2)
+            textObject.text += "rd";
+        else
+            textObject.text += "th";
 
-    
+    }
+
+
 }
