36a3c1cf5baadc33dc11368711c611337f707c93
Marius Urbelis <marius@urbelis.dev>
2020-10-18 14:19:23 +0100

    Powerups system development merge (#103)
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    * Powerup development
    
    Co-authored-by: ch6li3 <34590149+ch6li3@users.noreply.github.com>

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,154 +1,183 @@
 ﻿using Photon.Pun;
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-	public string username = "";
-	public Color playerColor;
+    public string username = "";
+    public Color playerColor;
 
-	public PhotonView photonView;
-	public Rigidbody2D myBody;
-	private Animator animator;
-	private bool isDead = false;
+    public PhotonView photonView;
+    public Rigidbody2D myBody;
+    private Animator animator;
+    private bool isDead = false;
 
-	public int health = 3;
+    public Powerup.PowerupType powerup = Powerup.PowerupType.None;
 
-	public int selectedOption = -1;
+    public int health = 3;
 
-	private void Awake()
-	{
-		photonView = GetComponent<PhotonView>();
-		myBody = GetComponent<Rigidbody2D>();
-		animator = GetComponent<Animator>();
+    public int selectedOption = -1;
 
-
-		if (PlayerDataManager.ValueExists(PlayerDataManager.PlayerColor))
-		{
-			if (ColorUtility.TryParseHtmlString(PlayerDataManager.LoadData(PlayerDataManager.PlayerColor), out playerColor))
-			{
-				//Debug.Log("Player color successfully set");
-			}
-		}
-
-		if (PlayerDataManager.ValueExists(PlayerDataManager.PlayerUsername))
-		{
-			username = PlayerDataManager.LoadData(PlayerDataManager.PlayerUsername);
-		}
-
-		gameObject.AddComponent<PlayerAvatar>();
-		gameObject.AddComponent<PlayerID>();
-
-	}
-
-
-	/// <summary>
-	/// Informs the player that a round is done. Checks if player answered correctly and changes its health accordingly. If health reaches 0 player loses.
-	/// </summary>
-	public void RegisterRoundDone()
-	{
-		// return if dead ---------------
-		if (isDead) return;
-		//if(!IsMe) return;
-		//Debug.Log($"Player {username} selected {selectedOption} option");
-
-		// Selected option to check against the correct answer
-		if (selectedOption != QuizManager.currentCorrectAnswerID)
-		{
-			health--;
-			Sound.PlaySound(Sound.screamSound);
-			ToggleMovement(false);
-			animator.SetTrigger((selectedOption != -1) ? "Die" : "Melt");
-			StartCoroutine(BackToMiddle());
-			
-			if (health <= 0)
-			{
-				Die();
-			}
-		}
-	}
-
-	private IEnumerator BackToMiddle()
-	{
-		yield return new WaitForSeconds(1.5f);
-		gameObject.transform.position = Vector2.zero;
-		StartCoroutine(EnableMovementAfterTime());
-		yield return null;
-	}
-
-	private void ToggleMovement(bool active)
+    private void Awake()
     {
-		if (TryGetComponent(out PlayerMovement playerMovement))
-		{
-			playerMovement.enabled = active;
-		}
+        photonView = GetComponent<PhotonView>();
+        myBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+
+        if (PlayerDataManager.ValueExists(PlayerDataManager.PlayerColor))
+        {
+            if (ColorUtility.TryParseHtmlString(PlayerDataManager.LoadData(PlayerDataManager.PlayerColor), out playerColor))
+            {
+                //Debug.Log("Player color successfully set");
+            }
+        }
+
+        if (PlayerDataManager.ValueExists(PlayerDataManager.PlayerUsername))
+        {
+            username = PlayerDataManager.LoadData(PlayerDataManager.PlayerUsername);
+        }
+
+        gameObject.AddComponent<PlayerAvatar>();
+        gameObject.AddComponent<PlayerID>();
+
     }
 
-	private IEnumerator EnableMovementAfterTime()
+
+    /// <summary>
+    /// Informs the player that a round is done. Checks if player answered correctly and changes its health accordingly. If health reaches 0 player loses.
+    /// </summary>
+    public void RegisterRoundDone()
     {
-		yield return new WaitForSeconds(1.5f);
-		ToggleMovement(true);
-		yield return null;
+        // return if dead ---------------
+        if (isDead) return;
+        //if(!IsMe) return;
+        //Debug.Log($"Player {username} selected {selectedOption} option");
+
+        // Selected option to check against the correct answer
+        if (selectedOption != QuizManager.currentCorrectAnswerID)
+        {
+            health--;
+            Sound.PlaySound(Sound.screamSound);
+            ToggleMovement(false);
+            animator.SetTrigger((selectedOption != -1) ? "Die" : "Melt");
+            StartCoroutine(BackToMiddle());
+
+            if (health <= 0)
+            {
+                Die();
+            }
+        }
     }
 
-	/// <summary>
-	/// Player's movement is disabled and the player is taken back to the center of the game screen.
-	/// </summary>
-	private void Die()
-	{
-		//Adds players username to a list in the order they were eliminated
-		QuizManager.eliminationList.Add(username);
+    [PunRPC]
+    void RPC_BeAffectedByPowerup(Powerup.PowerupType powerupType, Vector2 powerupPosition)
+    {
+        Debug.Log("Got affected by a powerup");
+        switch (powerupType)
+        {
+            case Powerup.PowerupType.Bomb:
+                //myBody.AddForce(powerupPosition - (Vector2)transform.position, ForceMode2D.Impulse);
+                myBody.AddForce(Vector2.up * 20f, ForceMode2D.Impulse);
+                break;
+        }
+    }
 
-		//Not to be used in this method. example to be used in leaderboard scene
-		int listLength = QuizManager.eliminationList.Count;
-		for (int i = 0; i < listLength; i++)
-		{
-			//temporary addition for debug use
-			Debug.Log(QuizManager.eliminationList[i].ToString());
+    private IEnumerator BackToMiddle()
+    {
+        yield return new WaitForSeconds(1.5f);
+        gameObject.transform.position = Vector2.zero;
+        StartCoroutine(EnableMovementAfterTime());
+        yield return null;
+    }
 
-			//actual code to go in this would be for adding the names of people to the end leaderboard in the order they were eliminated
-			//does not have winner in the list currently
-		}
-		// Temporary
-		Destroy(gameObject.GetComponent<PlayerMovement>());
-		transform.position = new Vector2(0, -4f);
-		isDead = true;
-	}
+    private void ToggleMovement(bool active)
+    {
+        if (TryGetComponent(out PlayerMovement playerMovement))
+        {
+            playerMovement.enabled = active;
+        }
+    }
 
-	/// <summary>
-	/// Checks if the player object belongs to the player currently controling the game.
-	/// </summary>
-	public bool IsMe => photonView.IsMine;
+    private IEnumerator EnableMovementAfterTime()
+    {
+        yield return new WaitForSeconds(1.5f);
+        ToggleMovement(true);
+        yield return null;
+    }
 
-	/// <summary>
-	/// Returns the unique identifier of the player.
-	/// </summary>
-	public string PlayerID => photonView.ViewID.ToString();
+    public void UsePowerup()
+    {
+        Debug.Log($"{username} used a powerup");
 
-	/// <summary>
-	/// Returns the player username or the unique ID if the username field is empty.
-	/// </summary>
-	public string PlayerName => ((username.Length < 2) ? PlayerID : username);
+        foreach (Player player in FindObjectsOfType<Player>())
+        {
+            player.photonView.RPC("RPC_BeAffectedByPowerup", RpcTarget.All, powerup, (Vector2)transform.position);
+        }
 
-	/// <summary>
-	/// Returns the player object that belongs to the actual player.
-	/// </summary>
-	public static Player Me
-	{
-		get
-		{
-			foreach (Player player in FindObjectsOfType<Player>())
-			{
-				if (player.IsMe)
-				{
-					return player;
-				}
-			}
+        powerup = Powerup.PowerupType.None;
+    }
 
-			return null;
-		}
-	}
+    /// <summary>
+    /// Player's movement is disabled and the player is taken back to the center of the game screen.
+    /// </summary>
+    private void Die()
+    {
+        //Adds players username to a list in the order they were eliminated
+        QuizManager.eliminationList.Add(username);
+
+        //Not to be used in this method. example to be used in leaderboard scene
+        int listLength = QuizManager.eliminationList.Count;
+        for (int i = 0; i < listLength; i++)
+        {
+            //temporary addition for debug use
+            Debug.Log(QuizManager.eliminationList[i].ToString());
+
+            //actual code to go in this would be for adding the names of people to the end leaderboard in the order they were eliminated
+            //does not have winner in the list currently
+        }
+        // Temporary
+        Destroy(gameObject.GetComponent<PlayerMovement>());
+        transform.position = new Vector2(0, -4f);
+        isDead = true;
+    }
+
+    public void PickUpPowerup(Powerup.PowerupType powerupType)
+    {
+        powerup = powerupType;
+    }
+
+    /// <summary>
+    /// Checks if the player object belongs to the player currently controling the game.
+    /// </summary>
+    public bool IsMe => photonView.IsMine;
+
+    /// <summary>
+    /// Returns the unique identifier of the player.
+    /// </summary>
+    public string PlayerID => photonView.ViewID.ToString();
+
+    /// <summary>
+    /// Returns the player username or the unique ID if the username field is empty.
+    /// </summary>
+    public string PlayerName => ((username.Length < 2) ? PlayerID : username);
+
+    /// <summary>
+    /// Returns the player object that belongs to the actual player.
+    /// </summary>
+    public static Player Me
+    {
+        get
+        {
+            foreach (Player player in FindObjectsOfType<Player>())
+            {
+                if (player.IsMe)
+                {
+                    return player;
+                }
+            }
+
+            return null;
+        }
+    }
 }
---
+++ Assets/Scripts/Powerups/Powerup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    public enum PowerupType { None, Bomb, Half, Fan, Hint, Ice, LightsOut, Star, WildCard };
+
+    [SerializeField] private PowerupType myType;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (collision.gameObject.GetComponent<Player>().powerup == PowerupType.None)
+            {
+                collision.gameObject.GetComponent<Player>().PickUpPowerup(myType);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
