9941b80b3841bfdf06dc319bbded9f39229b9948
Costin Grigore <costingrigore6@gmail.com>
2020-10-19 13:06:04 +0100

    Added toggle sound functionality

--- Assets/Scripts/Sound/SoundtrackManager.cs
+++ Assets/Scripts/Sound/SoundtrackManager.cs
@@ -1,13 +1,40 @@
-﻿using System.Collections;
+﻿using ExitGames.Client.Photon;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
 
 public class SoundtrackManager : MonoBehaviour
 {
 	public static SoundtrackManager instance;
+	[SerializeField] private Button audioBtn = null;
+	[SerializeField] private GameObject audioBtnGameObject = null;
+
+	public static AudioSource audioSource;
+
+	public static AudioClip soundTrackSound;
+
+	public AudioClip soundTrackSoundStub;
+
+	[SerializeField] private Sprite soundOnImage;
+
+	[SerializeField] private Sprite soundOffImage;
+
+	[SerializeField] private Image buttonImage;
 
 	void Awake()
 	{
+		audioSource = GetComponent<AudioSource>();
+
+		soundTrackSound = soundTrackSoundStub;
+
+		audioBtn.onClick.AddListener(() => ToggleSound());
+
+		buttonImage.GetComponent<Image>();
+
+		audioSource.PlayOneShot(soundTrackSound);
+
 		if (instance == null)
 			instance = this;
 		else
@@ -16,5 +43,20 @@ public class SoundtrackManager : MonoBehaviour
 			return;
 		}
 		DontDestroyOnLoad(gameObject);
+	}
+	private void ToggleSound()
+	{
+		if (audioSource.isPlaying)
+		{
+			audioSource.Stop();
+			buttonImage.sprite = soundOffImage;
+			Sound.soundOn = false;
+		}
+		else
+		{
+			audioSource.PlayOneShot(soundTrackSound);
+			buttonImage.sprite = soundOnImage;
+			Sound.soundOn = true;
+		}
 	}
 }
