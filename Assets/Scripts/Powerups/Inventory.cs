a4b72281ac48acfb4a9d4b20b4a060ac7e568e8b
ch6li3 <34590149+ch6li3@users.noreply.github.com>
2020-10-20 09:21:04 +0100

    Powerup development

--- Assets/Scripts/Powerups/Inventory.cs
+++ Assets/Scripts/Powerups/Inventory.cs
@@ -2,16 +2,41 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    private TextMeshProUGUI powerupText;
+  
+    public static Inventory instance;
 
+  
+    private TextMeshProUGUI powerupText;
+    public Image icon;
+    public Button removeButton;
     
+
+    public void AddPowerup(Sprite newPowerup)
+    {
+        Debug.Log("Sprite passed:" + newPowerup);
+          
+        icon.sprite = newPowerup;
+        Debug.Log("Sprite obtained:" + icon.sprite);
+        icon.enabled = true;
+        removeButton.interactable = true;
+    }
+
+    public void DropPowerup()
+    {
+        icon.sprite = null;
+        icon.enabled = false;
+        removeButton.interactable = false;
+    }
+
     void Start()
     {
         powerupText = GameObject.Find("PowerupText").GetComponent<TextMeshProUGUI>();
-        
+       
+       
     }
 
     void Update()
@@ -19,14 +44,25 @@ public class Inventory : MonoBehaviour
         if (Player.Me)
         {
             powerupText.text = "Player has: " + Player.Me.powerup.ToString();
-          
+            
         }
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            Player.Me.UsePowerup();
+
+            UsePowerup();
         }
     }
 
+    public void UsePowerup()
+    {
+        Player.Me.UsePowerup();
+        DropPowerup();
+    }
    
 }
+
+
+
+
+    
--- Assets/Scripts/Powerups/Powerup.cs
+++ Assets/Scripts/Powerups/Powerup.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Powerup : MonoBehaviour
 {
@@ -8,13 +11,30 @@ public class Powerup : MonoBehaviour
 
     [SerializeField] private PowerupType myType = PowerupType.None;
 
+    public Sprite powerUpIcon= null; //variable to store powerup Icons of the prefab
+    private Inventory inventory;
+
+    private void Start()
+    {
+        inventory = GameObject.Find("Logic").GetComponent<Inventory>();
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             if (collision.gameObject.GetComponent<Player>().powerup == PowerupType.None)
-            {
+            {   
                 collision.gameObject.GetComponent<Player>().PickUpPowerup(myType);
+                //Debug.Log("Collison with:" + myType);
+                //initialising gameobject prefab with the powerup picked up in game with the relevant powerup in the powerups folder prefab
+                GameObject powerIconPrefab = (GameObject)Resources.Load(Path.Combine("Powerups", myType.ToString()), typeof(GameObject));
+              
+                //Debug.Log("PowerupPrefab:" + powerIconPrefab);
+                powerUpIcon = powerIconPrefab.GetComponent<SpriteRenderer>().sprite;
+               // Debug.Log("PowerupIcon:" + powerUpIcon);
+                inventory.AddPowerup(powerUpIcon);
                 Destroy(gameObject);
             }
         }
