0abab33fd754e23549f48b28fd1905f60516c8da
ch6li3 <34590149+ch6li3@users.noreply.github.com>
2020-10-21 11:09:12 +0100

    Powerup development

--- Assets/Scripts/Powerups/Powerup.cs
+++ Assets/Scripts/Powerups/Powerup.cs
@@ -27,21 +27,23 @@ public class Powerup : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             if (collision.gameObject.GetComponent<Player>().powerup != PowerupType.None) return;
-            if (collision.gameObject.GetComponent<Player>().IsMe) return;
+            if (collision.gameObject.GetComponent<Player>().IsMe)
+            {
 
-            collision.gameObject.GetComponent<Player>().PickUpPowerup(myType);
-            //Debug.Log("Collison with:" + myType);
-            //initialising gameobject prefab with the powerup picked up in game with the relevant powerup in the powerups folder prefab
-            GameObject powerIconPrefab = (GameObject)Resources.Load(Path.Combine("Powerups", myType.ToString()), typeof(GameObject));
+                collision.gameObject.GetComponent<Player>().PickUpPowerup(myType);
+                //Debug.Log("Collison with:" + myType);
+                //initialising gameobject prefab with the powerup picked up in game with the relevant powerup in the powerups folder prefab
+                GameObject powerIconPrefab = (GameObject)Resources.Load(Path.Combine("Powerups", myType.ToString()), typeof(GameObject));
 
-            //Debug.Log("PowerupPrefab:" + powerIconPrefab);
-            powerUpIcon = powerIconPrefab.GetComponent<SpriteRenderer>().sprite;
+                //Debug.Log("PowerupPrefab:" + powerIconPrefab);
+                powerUpIcon = powerIconPrefab.GetComponent<SpriteRenderer>().sprite;
 
-            Debug.Log("PowerupIcon:" + powerUpIcon);
+                Debug.Log("PowerupIcon:" + powerUpIcon);
 
-            inventory.AddPowerup(powerUpIcon);
+                inventory.AddPowerup(powerUpIcon);
 
-            Destroy(gameObject);
+                Destroy(gameObject);
+            }
         }
     }
 }
