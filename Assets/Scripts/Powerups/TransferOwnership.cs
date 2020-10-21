39949d7e5355b9733b16f2f45b084170f0d5a26a
ch6li3 <34590149+ch6li3@users.noreply.github.com>
2020-10-21 14:02:37 +0100

    Powerup development

---
+++ Assets/Scripts/Powerups/TransferOwnership.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class TransferOwnership : MonoBehaviourPun, IPunOwnershipCallbacks
+{   
+    //run on enable
+    private void Awake()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    //run on disabled
+    private void OnDestroy()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public void OnOwnershipRequest(PhotonView targetView, Photon.Realtime.Player requestingPlayer)
+    {
+        if (targetView != base.photonView)
+        {
+            return;
+        }
+        base.photonView.TransferOwnership(requestingPlayer);
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Photon.Realtime.Player previousOwner)
+    {
+        if (targetView != base.photonView)
+        {
+            return;
+        }
+    }
+
+   
+
+    
+}
